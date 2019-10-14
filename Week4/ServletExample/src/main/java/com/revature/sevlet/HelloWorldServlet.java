7e7f4f100498739f7d9f8a35a20a76dd159483a7
jurczakn <nickolas.jurczak@gmail.com>
2019-10-14 16:17:37 -0400

    servlet example

---
+++ Week4/ServletExample/src/main/java/com/revature/sevlet/HelloWorldServlet.java
@@ -0,0 +1,28 @@
+package com.revature.sevlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		System.out.println("Hello World!!!!!!!!");
+		//throw new ServletException();
+		PrintWriter pw = resp.getWriter();
+		String position = req.getParameter("position");
+		String name = req.getParameter("name");
+		pw.write("<h1>Hello " + position + " " + name + "</h1>");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		System.out.println("Hello Post!!!!!");
+	}
+
+}
