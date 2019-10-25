cb8068ab0f0d73f8a4642f696d19933e0590c0ac
jurczakn <nickolas.jurczak@gmail.com>
2019-10-25 15:30:14 -0400

    hibernate mapping example

--- Week5/HibernateExample/src/main/java/com/revature/domains/HoneyPot.java
+++ Week5/HibernateExample/src/main/java/com/revature/domains/HoneyPot.java
@@ -1,5 +1,98 @@
 package com.revature.domains;
 
-public class HoneyPot {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="HONEYPOT")
+public class HoneyPot {
+	
+	@Id
+	@SequenceGenerator(name="HONEYPOTID_SEQ", sequenceName="HONEYPOTID_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "HONEYPOTID_SEQ")
+	@Column(name="HONEYPOT_ID")
+	private int honeyPotId;
+	
+	@Column(name="VOLUME")
+	private double volume;
+	
+	@Column(name="HONEYPOT_AMOUNT")
+	private double honeyAmount;
+
+	public int getHoneyPotId() {
+		return honeyPotId;
+	}
+
+	public void setHoneyPotId(int honeyPotId) {
+		this.honeyPotId = honeyPotId;
+	}
+
+	public double getVolume() {
+		return volume;
+	}
+
+	public void setVolume(double volume) {
+		this.volume = volume;
+	}
+
+	public double getHoneyAmount() {
+		return honeyAmount;
+	}
+
+	public void setHoneyAmount(double honeyAmount) {
+		this.honeyAmount = honeyAmount;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(honeyAmount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + honeyPotId;
+		temp = Double.doubleToLongBits(volume);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HoneyPot other = (HoneyPot) obj;
+		if (Double.doubleToLongBits(honeyAmount) != Double.doubleToLongBits(other.honeyAmount))
+			return false;
+		if (honeyPotId != other.honeyPotId)
+			return false;
+		if (Double.doubleToLongBits(volume) != Double.doubleToLongBits(other.volume))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "HoneyPot [honeyPotId=" + honeyPotId + ", volume=" + volume + ", honeyAmount=" + honeyAmount + "]";
+	}
+
+	public HoneyPot(int honeyPotId, double volume, double honeyAmount) {
+		super();
+		this.honeyPotId = honeyPotId;
+		this.volume = volume;
+		this.honeyAmount = honeyAmount;
+	}
+
+	public HoneyPot() {
+		super();
+	}
+	
 }
--- Week5/HibernateExample/src/main/java/com/revature/driver/HibernateDriver.java
+++ Week5/HibernateExample/src/main/java/com/revature/driver/HibernateDriver.java
@@ -1,13 +1,32 @@
 package com.revature.driver;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import com.revature.domains.HoneyPot;
 import com.revature.util.SessionFactoryUtil;
 
 public class HibernateDriver {
 	
+	private static SessionFactory sf = SessionFactoryUtil.getSessionFactory();
+	
 	public static void main(String[] args) {
-		SessionFactory sf = SessionFactoryUtil.getSessionFactory();
+		Session sess = sf.openSession();
+		Transaction tx = sess.beginTransaction();
+		HoneyPot hp1 = (HoneyPot) sess.get(HoneyPot.class, 1);
+		System.out.println(hp1);
+		hp1.setVolume(2000.0);
+		sess.update(hp1);
+		//HoneyPot hp2 = new HoneyPot(-1, 99.9, 11.1);
+		//sess.save(hp2);
+		HoneyPot hp2 = (HoneyPot) sess.get(HoneyPot.class, 200);
+		sess.delete(hp2);
+		tx.commit();
+		sess.close();
+		System.out.println("everything is fine");
+		sf.close();
+		
 	}
 
 }
